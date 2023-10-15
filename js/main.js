9ecbe63b943e2f17fbac126ebef85657dab48e12
SHADI <123980009+SHADI-i0@users.noreply.github.com>
2023-10-15 08:48:44 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
-// Letters
 const Letters = "abcdefghijklmnopqrstuvwxyz";
-// Get Array From Letters
 const LettersArray = Array.from(Letters);
-// Select Letters Container
 let LettersContainer = document.querySelector(".letters");
-// Generate Letters
 LettersArray.forEach((letter) => {
     let span = document.createElement("span");
     let theLetter = document.createTextNode(letter);
@@ -12,68 +8,45 @@ LettersArray.forEach((letter) => {
     span.className = "letter-box";
     LettersContainer.appendChild(span);
 });
-// Object Of Words + Categories
+
 const words = {
     programming: ["php", "javascript", "go", "scala", "my sql", "python", "react", "Next js", "fortran", "C", "java", "node js", "express js", "mongodb", "Nest js", ],
     SocialMedia: ["Whats App", "Face book", "Instagram", "Telegram", "Messenger", "Linked In", "YouTube", "snapChat", ],
     names: ["AbdAlrahman", "Shadi", "Maya", "Louay", "abd allh", "Nehad", "Melad", "Mohammed", "Ahmed", ],
     countries: ["syria", "palestine", "yemen", "egypt", "bahrain", "Qatar", "lebanon", "ksa", "jordan", "Iraq", ],
 };
-// Get Random Property
+
 let allKeys = Object.keys(words);
-// Random Number Depend On Keys Length
 let randomPropNumber = Math.floor(Math.random() * allKeys.length);
-// Category
 let randomPropName = allKeys[randomPropNumber];
-// Category words
 let randomPropValue = words[randomPropName];
-// Random Number Depend On words
 let randomValueNumber = Math.floor(Math.random() * randomPropValue.length);
-// The chosen Word
 let randomValueValue = randomPropValue[randomValueNumber];
-// set category Info
 document.querySelector(".category span").innerHTML = randomPropName; // + " , " + randomValueValue
 
-// select Letters Guess Container
 let lettersGuessContainer = document.querySelector(".letters-guess");
-// Convert Chosen Word To array
 let lettersAndSpace = Array.from(randomValueValue);
-// Create Spans Depend On word
 lettersAndSpace.forEach((letter) => {
-    //create empty span
     let span = document.createElement("span");
-    // if letter is space
     if (letter === " ") {
-        // Add Class To span
         span.className = "with-space";
     }
-    //Append Spans To The Letters Guess Container
     lettersGuessContainer.appendChild(span);
 });
-// select Guess Spans
-let guessSpans = document.querySelectorAll(".letters-guess span");
 
-// set Wrong Attempts
+let guessSpans = document.querySelectorAll(".letters-guess span");
 let wrongAttempts = 0;
 let winnerAttempts = 0;
-
-// select The Draw Element
 let theDraw = document.querySelector(".hangman-draw");
 
-// Handle Clicking On letters
 document.addEventListener("click", function (e) {
-    // set the chose status
     let theStatus = false;
     if (e.target.className === "letter-box") {
         e.target.classList.add("clicked");
-        //Get clicked letter
         let theClickedLetter = e.target.innerHTML.toLowerCase();
-        // The Chosen Word
         let theChosenWord = Array.from(randomValueValue.toLowerCase());
         theChosenWord.forEach((wordLetter, wordIndex) => {
-            // If the clicked letter equal To one of the chosen word letter
             if (theClickedLetter == wordLetter) {
-                // Set status To correct
                 theStatus = true;
                 guessSpans.forEach((span, spanIndex) => {
                     if (wordIndex == spanIndex) {
@@ -81,18 +54,16 @@ document.addEventListener("click", function (e) {
                         winnerAttempts++;
                         if (winnerAttempts == theChosenWord.length) {
                             winner();
+                        }else if (winnerAttempts == theChosenWord.length - 1 && theChosenWord.includes(" ")){
+                            winner()
                         }
                     }
                 });
             }
         });
-        // If Letter Is Wrong
         if (theStatus !== true) {
-            // Increase The Wrong Attempts
             wrongAttempts++;
-            // Add Class Wrong On The Draw Element
             theDraw.classList.add(`wrong-${wrongAttempts}`);
-            // play Fail Sound
             document.getElementById("fail").play();
             if (wrongAttempts === 8) {
                 endGame();
@@ -103,9 +74,8 @@ document.addEventListener("click", function (e) {
         }
     }
 });
-// End Game Function
+
 function endGame() {
-    // Create Popup Div
     let div = document.createElement("div");
     div.className = "popup";
     let divText = document.createTextNode(
@@ -122,7 +92,7 @@ function endGame() {
         window.location.reload();
     };
 }
-// winner Function
+
 function winner() {
     let div = document.createElement("div");
     div.className = "winner";
